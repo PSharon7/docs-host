47619ff842b577e047148284171462899238079d
Yufei Huang <yufeih@live.com>
2018-06-23 10:08:30 +0800

    store -> host

--- host.cs
+++ host.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
@@ -10,13 +11,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Newtonsoft.Json;
 
-static class Store
+static class Host
 {
     class Document
     {
@@ -30,6 +32,8 @@ static class Store
 
     static readonly DocumentClient s_db = new DocumentClient(new Uri(""), "");
     static readonly CloudBlobContainer s_blob = CloudStorageAccount.Parse("").CreateCloudBlobClient().GetRootContainerReference();
+
+    static readonly Task<string> s_spFindDocs = CreateStoredProcedure("finddocs", "SELECT * FROM docs");
 
     static void Main(string[] args) => WebHost.CreateDefaultBuilder(args).Configure(Configure).Build().Run();
 
@@ -45,7 +49,8 @@ static class Store
     static async Task FindDoc(HttpContext http)
     {
         var filter = await http.ReadAs<Dictionary<string, string>>();
-        var result = await s_db.ExecuteStoredProcedureAsync("", (dynamic)null);
+        var spFindDocs = await s_spFindDocs;
+        var result = await s_db.ExecuteStoredProcedureAsync(spFindDocs, (dynamic)null);
         await http.Write((Document[])result.Response);
     }
 
@@ -57,16 +62,13 @@ static class Store
 
     static async Task PutBlob(HttpContext http)
     {
-        var bytes = new MemoryStream();
-        await http.Request.Body.CopyToAsync(bytes);
-        using (var sha1 = SHA1.Create())
-        {
-            bytes.Position = 0;
-            var hash = WebEncoders.Base64UrlEncode(sha1.ComputeHash(bytes));
-            bytes.Position = 0;
-            await s_blob.GetBlockBlobReference(hash).UploadFromStreamAsync(bytes);
-            await http.Response.WriteAsync(hash);
-        }
+        var stream = new MemoryStream();
+        await http.Request.Body.CopyToAsync(stream);
+        stream.Position = 0;
+        var hash = Hash(stream);
+        stream.Position = 0;
+        await s_blob.GetBlockBlobReference(hash).UploadFromStreamAsync(stream);
+        await http.Response.WriteAsync(hash);
     }
 
     static async Task HasBlob(HttpContext http)
@@ -79,4 +81,27 @@ static class Store
     static async Task<T> ReadAs<T>(this HttpContext http) => JsonConvert.DeserializeObject<T>(await new StreamReader(http.Request.Body).ReadToEndAsync());
 
     static Task Write<T>(this HttpContext http, T value) => http.Response.WriteAsync(JsonConvert.SerializeObject(value));
+
+    static string Hash(Stream stream)
+    {
+        using (var sha1 = SHA1.Create())
+        {
+            return WebEncoders.Base64UrlEncode(sha1.ComputeHash(stream));
+        }
+    }
+
+    static string Hash(string str)
+    {
+        using (var sha1 = SHA1.Create())
+        {
+            return WebEncoders.Base64UrlEncode(sha1.ComputeHash(Encoding.UTF8.GetBytes(str)));
+        }
+    }
+
+    static async Task<string> CreateStoredProcedure(string name, string code)
+    {
+        var link = $"{name}-{Hash(code)}";
+        await s_db.CreateStoredProcedureAsync(link, new StoredProcedure { Body = code });
+        return link;
+    }
 }
