dc8264b58ea30362b141318cad4dc83e3a946be2
Sharon P <Psharon7@hotmail.com>
2018-07-03 17:02:50 +0800

    add

---
+++ Document.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace docs.host
+{
+    public class Document
+    {
+        public string url
+        { get; set; }
+        public string blob
+        { get; set; }
+        public string locale
+        { get; set; }
+        public string version
+        { get; set; }
+        public string commit
+        { get; set; }
+    }
+}
--- host.cs
+++ host.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
@@ -21,30 +22,19 @@ using Newtonsoft.Json;
 
 static class Host
 {
-    public class Document
-    {
-        public string url;
-        public string blob;
-        public string locale;
-        public string version;
-        public string commit;
-    }
 
-    private const string EndpointUrl = "https://docs-host.documents.azure.com:443/";
-    private const string PrimaryKey = "VmEAHupcmH3hc0VccBmR2jsh7viNtHBh6UX4OkoDtQ3sMX9HRvgD8tFXR1yObtyFHfFmh6c4fzBvuVXSh7Wrzw==";
-    private const string defaultBranch = "master";
+    private readonly static string CollectionDocument = ConfigurationManager.AppSettings["collectionDoc"];
+    private readonly static string CollectionBase = ConfigurationManager.AppSettings["collectionBase"];
+    
 
-    private readonly static string DatabaseName = "docs-test";
-    private readonly static string CollectionDocument = "Documents";
-    private readonly static string CollectionBase = "BaseInfo";
-
-    static readonly DocumentClient s_db = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
     //static readonly CloudBlobContainer s_blob = CloudStorageAccount.Parse("").CreateCloudBlobClient().GetRootContainerReference();
-
-    static readonly Task<string> s_spFindDocs = CreateStoredProcedure("finddocs", "SELECT * FROM docs");
+    
 
     static void Main(string[] args)
     {
+        DocumentDBRepo<BaseInfo>.Initialize(CollectionBase);
+        DocumentDBRepo<docs.host.Document>.Initialize(CollectionDocument);
+
         WebHost.CreateDefaultBuilder(args)
             .ConfigureServices(services => services.AddRouting())
             .Configure(Configure)
@@ -55,8 +45,8 @@ static class Host
     static void Configure(IApplicationBuilder app) => app.UseRouter(BuildRoutes(app));
 
     static IRouter BuildRoutes(IApplicationBuilder app) => new RouteBuilder(app)
-        .MapGet("finddoc", FindDoc)
-        //.MapPut("doc", PutDoc)
+        .MapPost("finddoc", FindDoc)
+        .MapPut("doc", PutDoc)
         //.MapPost("hasblob", HasBlob)
         //.MapPut("blob", PutBlob)
         .Build();
@@ -64,18 +54,47 @@ static class Host
     static async Task FindDoc(HttpContext http)
     {
         var filter = await http.ReadAs<Dictionary<string, string>>();
-        List<string> commits = QueryCommitByBasename("/azure", "");
+        var baseinfos = await DocumentDBRepo<BaseInfo>.GetItemsAsync(b => b.basename == "/azure" && b.branch == "master");
+        List<string> commits = new List<string>();
+        foreach (BaseInfo b in baseinfos)
+        {
+            commits.Add(b.commit);
+        }
 
-        var spFindDocs = await s_spFindDocs;
-        var result = await s_db.ExecuteStoredProcedureAsync(spFindDocs, (dynamic)null);
-        await http.Write((Document[])result.Response);
+        var documents = await DocumentDBRepo<docs.host.Document>.GetItemsAsync(d => commits.Contains(d.commit));
+
+        await http.Write(documents);
+    }
+    
+
+    static async Task PutDoc(HttpContext http)
+    {
+        var docs = await http.ReadAs<docs.host.Document[]>();
+        docs.host.Document d1 = new docs.host.Document();
+        d1.url = "/azure/new1";
+        d1.blob = "Empty";
+        d1.locale = "zh-cn";
+        d1.version = "1";
+        d1.commit = "123";
+
+        docs.host.Document d2 = new docs.host.Document();
+        d2.url = "/azure/new2";
+        d2.blob = "Empty";
+        d2.locale = "zh-cn";
+        d2.version = "2";
+        d2.commit = "124";
+
+        var temp = new docs.host.Document[] { d1, d2 };
+
+        await Task.WhenAll(temp.Select(doc => DocumentDBRepo<docs.host.Document>.UpsertItemsAsync(doc)));
     }
 
     /*
-    static async Task PutDoc(HttpContext http)
+    static async Task HasBlob(HttpContext http)
     {
-        var docs = await http.ReadAs<Document[]>();
-        await Task.WhenAll(docs.Select(doc => s_db.UpsertDocumentAsync("", doc, disableAutomaticIdGeneration: true)));
+        var hashes = await http.ReadAs<string[]>();
+        var exists = await Task.WhenAll(hashes.Select(hash => s_blob.GetBlobReference(hash).ExistsAsync()));
+        await http.Write(exists);
     }
 
     static async Task PutBlob(HttpContext http)
@@ -87,13 +106,6 @@ static class Host
         stream.Position = 0;
         await s_blob.GetBlockBlobReference(hash).UploadFromStreamAsync(stream);
         await http.Response.WriteAsync(hash);
-    }
-
-    static async Task HasBlob(HttpContext http)
-    {
-        var hashes = await http.ReadAs<string[]>();
-        var exists = await Task.WhenAll(hashes.Select(hash => s_blob.GetBlobReference(hash).ExistsAsync()));
-        await http.Write(exists);
     }
     */
 
@@ -117,43 +129,12 @@ static class Host
         }
     }
 
+    /*
     static async Task<string> CreateStoredProcedure(string name, string code)
     {
         var link = $"{name}-{Hash(code)}";
         await s_db.CreateStoredProcedureAsync(link, new StoredProcedure { Body = code });
         return link;
     }
-
-    static List<string> QueryCommitByBasename(string basename, string branch)
-    {
-        if (string.IsNullOrWhiteSpace(branch))
-        {
-            branch = defaultBranch;
-        }
-
-        IQueryable<BaseInfo> query = s_db.CreateDocumentQuery<BaseInfo>(
-            UriFactory.CreateDocumentCollectionUri(DatabaseName, CollectionBase), 
-            $"SELECT * FROM BaseInfo b WHERE b.basement = '{basename}'",
-            new FeedOptions { });
-
-        List<string> commits = new List<string>();
-        foreach (BaseInfo b in query)
-        {
-            commits.Add(b.commit);
-        }
-        
-        return commits;
-    }
-
-    static IQueryable<Document> QureyDocument(string locale, string assetID, string version, string blob, List<string> commit)
-    {
-        var query = s_db.CreateDocumentQuery(
-            UriFactory.CreateDocumentCollectionUri(DatabaseName, CollectionDocument),
-            $"SELECT * " +
-            $"FROM Documents d " +
-            $"WHERE d.locale == '{locale}' AND d.version = '{version}' AND d.blob = '{blob}' AND d.assetID = '{assetID}' AND d.commit in '{commit}'",
-            new FeedOptions()
-            );
-        return (IQueryable<Document>)query;
-    }
+    */
 }
