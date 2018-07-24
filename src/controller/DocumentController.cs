2384598a105b74c1487ff2186b6776593f486298
Wei Hu <huwe@microsoft.com>
2018-07-24 10:50:42 +0800

    Add document controller

---
+++ src/controller/DocumentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace docs.host
+{
+    [Route("api/Documents")]
+    [ApiController]
+    public class DocumentController : ControllerBase
+    {
+        [HttpGet("{url}/{branch}/{locale}/{version}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<Document>> Get(string url, string branch, string locale, string version)
+        {
+            var document = await Reader.QueryDocument(url, branch, locale, version);
+            if (document is null)
+                return NotFound();
+
+            return document;
+        }
+    }
+}
