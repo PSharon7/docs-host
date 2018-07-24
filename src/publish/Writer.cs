513f5dc861660eb39e2742d66146988a80491b7e
Sharon P <Psharon7@hotmail.com>
2018-07-24 10:40:32 +0800

    uploadPage

--- src/publish/Writer.cs
+++ src/publish/Writer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -7,9 +8,44 @@ namespace docs.host
 {
     public static class Writer
     {
-        public static Task<(string pageUrl, string pageHash)> UploadPage(Stream pageStream, string pagePath)
+        public static async Task<(string pageUrl, string pageHash)> UploadPage(Stream pageStream, string contentType)
         {
-            throw new NotImplementedException();
+            string pageUrl;
+            string hash = HashUtility.GetSha1HashString(pageStream);            
+            
+            /* img or pdf*/
+            if (contentType == "png" || contentType == "jpg" || contentType == "pdf")
+            {
+                bool blobExist = await BlobAccessor.cloudBlobContainer.GetBlockBlobReference(hash).ExistsAsync();
+
+                if (!blobExist)
+                { 
+                    await BlobAccessor.cloudBlobContainer.GetBlockBlobReference(hash).UploadFromStreamAsync(pageStream);
+                }
+
+                pageUrl = ConfigurationManager.AppSettings["schema"] + ConfigurationManager.AppSettings["blob_domain"] + ConfigurationManager.AppSettings["blob_endpoint"] + hash;
+            }
+            else
+            {
+                ICollection<Page> pageExist = (ICollection<Page>)CosmosDBAccessor<Page>.QueryAsync(p => p.Hash == hash);
+                if (pageExist.Count == 0)
+                {
+                    Page page = new Page()
+                    {
+                        Hash = hash,
+                        Content = pageStream.ToString()
+                    };
+
+                    await CosmosDBAccessor<Page>.UpsertAsync(page);
+                }
+
+                pageUrl = ConfigurationManager.AppSettings["schema"] + ConfigurationManager.AppSettings["cosmos_domain"] + ConfigurationManager.AppSettings["cosmos_endpoint"] + hash;
+
+            }
+
+            pageStream.Close();
+            return (pageUrl, hash);
+            
         }
 
         public static Task UploadDocuments(List<Document> documents, Action<int, int> progress)
